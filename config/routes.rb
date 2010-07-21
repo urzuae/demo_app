2b5d2e4e4f797c143f18f115b734a31362521d36
Urzuae <urzuae@gmail.com>
2010-07-21 18:31:38 -0500

    Erase default index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :users
   map.resources :microposts
+  
+  map.root :users
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:

b2b7810cf8dab5ca94726ac409fe342bd01a5437
Urzuae <urzuae@gmail.com>
2010-07-21 19:11:41 -0500

    Add validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,8 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   validates_length_of :content, :maximum => 140
+  validates_presence_of :user_id
+  validates_presence_of :content
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  
+  validates_presence_of :name, :email
 end

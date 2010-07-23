dfbdc703d7f403703f851cac3caec4b2335267cd
Urzuae <urzuae@gmail.com>
2010-07-23 13:53:31 -0500

    Fix problem with user validation

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :find_micropost, :only => [:show, :edit, :update, :destroy]
+  before_filter :find_users, :only => [:create, :edit, :new, :update]
   
   def index
     @microposts = Micropost.all
@@ -9,7 +10,6 @@ class MicropostsController < ApplicationController
   end
   def new
     @micropost = Micropost.new
-    @users = User.all
     if @users.empty?
       flash[:notice] = 'Create a user first'
       redirect_to new_user_path
@@ -20,16 +20,17 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to @micropost
     else
+      flash[:error] = 'Micropost could not be created'
       render 'new'
     end
   end
   def edit
-    @users = User.all
   end
   def update
     if @micropost.update_attributes(params[:micropost])
       redirect_to @micropost
     else
+      flash[:error] = 'Micropost could not be updated'
       render 'edit'
     end
   end
@@ -48,5 +49,8 @@ class MicropostsController < ApplicationController
     @micropost = Micropost.find(params[:id])
   end
   
+  def find_users
+    @users = User.all
+  end
 
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,8 +2,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   validates_length_of :content, :maximum => 140
-  validates_presence_of :user_id
-  validates_presence_of :content
+  validates_presence_of :content, :user_id
 
   acts_as_taggable_on :tags
 

56bb12c9100ff3e69a91e965103172d88ba2798d
Urzuae <urzuae@gmail.com>
2010-07-26 15:08:41 -0500

    Add menu to layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,22 @@
 $(function() {
   
-
+  $('#users').hover(
+    function() {
+      $('.users').show();
+    },
+    function() {
+      $('.users').hide();
+    }
+  );
+  
+  $('#microposts').hover(
+    function() {
+      $('.microposts').show ();
+    },
+    function() {
+      $('.microposts').hide();
+    }
+  );
+  
   
 });

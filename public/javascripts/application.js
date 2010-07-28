1766ee2370d03b197798b32962ca5b8f3eb3abac
Urzuae <urzuae@gmail.com>
2010-07-27 19:00:52 -0500

    Make changes on db

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,2 @@
 $(function() {
-  
-  $('#users').hover(
-    function() {
-      $('.users').show();
-    },
-    function() {
-      $('.users').hide();
-    }
-  );
-  
-  $('#microposts').hover(
-    function() {
-      $('.microposts').show ();
-    },
-    function() {
-      $('.microposts').hide();
-    }
-  );
-  
-  
 });
